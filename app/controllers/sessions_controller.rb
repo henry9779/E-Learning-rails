a9aea97052835f46e23d53c61c62c098917c1b91
Henry Huang <73519702+henry9779@users.noreply.github.com>
2022-05-01 18:55:36 +0800

    feat: 新增 登出/登入 功能

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Login sucessful'
+    else
+      flash[:alert] = 'Invalid email or password'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Logout sucessful'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@
 
 Rails.application.routes.draw do
   root to: 'home#index'
+
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+
+  get 'log_in', to: 'sessions#new'
+  post 'log_in', to: 'sessions#create'
+  delete 'log_out', to: 'sessions#destroy'
 end
