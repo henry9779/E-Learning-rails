8fba4357a11ea161b6136ca544d5cb2287daf908
Henry Huang <73519702+henry9779@users.noreply.github.com>
2022-05-01 19:18:48 +0800

    feat: 新增 Current.user 方法，判斷用戶是否登入更換 navbar 註冊/登入 或 登出 顯示

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,7 +8,7 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: 'Sucessed'
+      redirect_to root_path, notice: 'Signup successful'
     else
       render :new
     end
