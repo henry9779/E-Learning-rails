c59d84b4a56e2be435126a3d2204ac586f5f4de4
Henry Huang <73519702+henry9779@users.noreply.github.com>
2022-05-01 21:50:48 +0800

    feat: navbar 新增 中/英文 切換功能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
+  before_action :set_current_user, :set_locale
+
+  private
 
   def set_current_user
     Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def set_locale
+    session[:locale] = params[:locale] if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
+
+    I18n.locale = session[:locale] || I18n.default_locale
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,5 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+I18n.available_locales = %w[en zh-TW]
