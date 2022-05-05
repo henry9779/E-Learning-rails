5012843bd254b93f9215550def3e363a46335925
Henry Huang <73519702+henry9779@users.noreply.github.com>
2022-05-05 20:07:21 +0800

    feat: base_controller 增加 authenticate_user 判斷是否有登入 feat: 若用戶 為 admin，navbar 增加後台管理 連結 refactor: 更改 i18n 用法 ( 符號改為字串 )

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,7 +8,7 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, notice: I18n.t(:signup_successful)
+      redirect_to root_path, notice: t('.success')
     else
       render :new
     end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,15 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: I18n.t(:login_sucessful)
+      redirect_to root_path, notice: t('.success')
     else
-      flash[:alert] = I18n.t(:invalid_email_or_password)
+      flash[:alert] = t('.fail')
       render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: I18n.t(:logout_sucessful)
+    redirect_to root_path, notice: t('.success')
   end
 end
