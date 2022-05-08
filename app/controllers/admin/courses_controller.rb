f3d56697d923f3f972aeb2149bc4eab7e1ceb3e7
Henry Huang <73519702+henry9779@users.noreply.github.com>
2022-05-08 13:34:27 +0800

    feat: 新增 課程管理後台     1. 可以執行CRUD基本操作     2. 若使用者不是管理員，則不允許操作     3. 可以設定課程主題     4. 可以設定課程價格，幣別     5. 可以設定課程類型     6. 可以設定課程上下架     7. 可以設定課程URL，以及描述     8. 可以設定課程效期 1天 ~ 1個月

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -1,5 +1,61 @@
 # frozen_string_literal: true
 
 class Admin::CoursesController < Admin::BaseController
-  def index; end
+  before_action :find_course, only: %i[show edit update destroy]
+
+  def index
+    @courses = Course.all
+  end
+
+  def show; end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      redirect_to admin_root_path, notice: t('.success')
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @course.update(course_params)
+      redirect_to admin_root_path, notice: t('.success')
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @course.destroy
+      redirect_to admin_root_path, notice: t('.success')
+    else
+      flash[:alert] = t('.fail')
+      render :index
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:subject,
+                                   :price,
+                                   :currency,
+                                   :category,
+                                   :status,
+                                   :url,
+                                   :description,
+                                   :valid_day)
+  end
+
+  def find_course
+    @course = Course.find_by(id: params[:id])
+  end
 end
