40918e62a00494b5874dd0e853198f123eb3b03f
Henry Huang <73519702+henry9779@users.noreply.github.com>
2022-05-11 00:38:33 +0800

    feat: 首頁 增加 顯示 已上架 課程， navbar 增加 已購買課程 連結 及 view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  before_action :init_course, only: :index
+  before_action :set_my_course, only: :my_courses
+
   def index; end
+
+  def my_courses; end
+
+  def init_course
+    @courses = Course.status_published
+  end
+
+  def set_my_course
+    user_course = Current.user.purchased_courses
+    course_ids = user_course.map(&:course_id)
+    @courses = Course.where(id: course_ids)
+  end
 end
